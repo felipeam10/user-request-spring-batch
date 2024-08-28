7df0fb3d270eaec7d78cf167e1a051bb417a55c0
felipeam10 <felipeam10@hotmail.com>
2024-08-28 10:18:27 -0300

    first commit

---
+++ src/main/java/com/felipeam10/user_request_spring_batch/job/JobConfig.java
@@ -0,0 +1,30 @@
+package com.felipeam10.user_request_spring_batch.job;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class JobConfig {
+
+    private static Logger logger = LoggerFactory.getLogger(JobConfig.class);
+    @Bean
+    public Job job(JobRepository jobRepository, Step fetchUserDataAndStoreDBStore) {
+
+        logger.info("Starting Job execution...");
+
+        return new JobBuilder("job", jobRepository)
+                .start(fetchUserDataAndStoreDBStore)
+                .incrementer(new RunIdIncrementer())
+                .build();
+    }
+
+}
