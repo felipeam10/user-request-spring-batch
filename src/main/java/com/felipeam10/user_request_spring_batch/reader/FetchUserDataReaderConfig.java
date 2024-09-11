7d997ffc7521a55fdac2ce6f15a91a7f7bba6d21
felipeam10 <felipeam10@hotmail.com>
2024-09-11 15:53:33 -0300

    vJSR-10.18 - paged reading part 04

--- src/main/java/com/felipeam10/user_request_spring_batch/reader/FetchUserDataReaderConfig.java
+++ src/main/java/com/felipeam10/user_request_spring_batch/reader/FetchUserDataReaderConfig.java
@@ -2,10 +2,16 @@ package com.felipeam10.user_request_spring_batch.reader;
 
 import com.felipeam10.user_request_spring_batch.domain.ResponseUser;
 import com.felipeam10.user_request_spring_batch.dto.UserDTO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.annotation.AfterChunk;
+import org.springframework.batch.core.annotation.BeforeChunk;
+import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.NonTransientResourceException;
 import org.springframework.batch.item.ParseException;
 import org.springframework.batch.item.UnexpectedInputException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
@@ -18,13 +24,16 @@ import java.util.List;
 @Configuration
 public class FetchUserDataReaderConfig implements ItemReader<UserDTO> {
 
+    private static Logger logger = LoggerFactory.getLogger(FetchUserDataReaderConfig.class);
     private final String BASE_URL = "http://localhost:8089";
     private RestTemplate restTemplate = new RestTemplate();
     private int page = 0;
-    private int size = 10;
-
     private List<UserDTO> users = new ArrayList<>();
     private int userIndex = 0;
+    @Value("${chunkSize}")
+    private int chunkSize;
+    @Value("${pageSize}")
+    private int pageSize;
 
     @Override
     public UserDTO read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
@@ -43,7 +52,10 @@ public class FetchUserDataReaderConfig implements ItemReader<UserDTO> {
 
         String uri =  BASE_URL + "/clients/pagedData?page=%d&size=%d";
 
-        ResponseEntity<ResponseUser> response = restTemplate.exchange(String.format(uri, getPage(), getSize()), HttpMethod.GET, null,
+        logger.info("[READER STEP] Fetching data...");
+        logger.info("[READER STEP] Request uri:" + String.format(uri, getPage(), pageSize));
+
+        ResponseEntity<ResponseUser> response = restTemplate.exchange(String.format(uri, getPage(), pageSize), HttpMethod.GET, null,
                 new ParameterizedTypeReference<ResponseUser>(){
                 });
 
@@ -56,8 +68,22 @@ public class FetchUserDataReaderConfig implements ItemReader<UserDTO> {
         return page;
     }
 
-   public int getSize() {
-        return size;
+    public void incrementPage() {
+        this.page++;
     }
 
+    @BeforeChunk
+    public void beforeChunk(ChunkContext context) throws Exception {
+        for (int i = 0; i < chunkSize; i += pageSize) {
+            users.addAll(fecthUserDataFromAPI());
+        }
+    }
+
+    @AfterChunk
+    public void afterChunk(ChunkContext context) throws Exception {
+        logger.info("Final chunk");
+        incrementPage();
+        userIndex = 0;
+        users = new ArrayList<>(); // clear list
+    }
 }
